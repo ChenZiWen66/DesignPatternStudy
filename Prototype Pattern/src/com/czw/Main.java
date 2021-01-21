fb6283d21ee4a7f53be5888a9cea2363be525ffe
chenziwen <401030526@qq.com>
2021-01-21 17:30:52 +0800

    1.写了一个原型模式的例子

--- Prototype Pattern/src/com/czw/Main.java
+++ Prototype Pattern/src/com/czw/Main.java
@@ -7,11 +7,15 @@ public class Main {
         ShapeCache.loadCache();
 
         Shape clonedShape = ShapeCache.getShape("1");
+        Shape clonedShape4 = ShapeCache.getShape("1");
         Shape clonedShape2 = ShapeCache.getShape("2");
         Shape clonedShape3 = ShapeCache.getShape("3");
 
         System.out.println("Shape:"+clonedShape.getType());
+        System.out.println("Shape:"+clonedShape.getId());
         System.out.println("Shape:"+clonedShape2.getType());
         System.out.println("Shape:"+clonedShape3.getType());
+        clonedShape.setId("999");
+        System.out.println(clonedShape4.getId());
     }
 }
