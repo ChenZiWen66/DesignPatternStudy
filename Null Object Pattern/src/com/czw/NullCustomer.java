50ce525727321c58cd6224cfcc4645452d1bf1d1
chenziwen <401030526@qq.com>
2021-02-02 11:53:13 +0800

    1.写了空对象模式的例子

---
+++ Null Object Pattern/src/com/czw/NullCustomer.java
@@ -0,0 +1,13 @@
+package com.czw;
+
+public class NullCustomer extends AbstractCustomer {
+    @Override
+    public String getName() {
+        return "Not Available in Customer Database";
+    }
+
+    @Override
+    public boolean isNil() {
+        return true;
+    }
+}
